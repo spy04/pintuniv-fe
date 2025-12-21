1f5a3f6674ae364d36b9428a360a57682d1069fb
Sigit <sigitprayogo392@gmail.com>
2025-12-21 23:35:03 +0700

    style: fix wording in subs

--- src/components/layout/rightcontainer/RightContainer.tsx
+++ src/components/layout/rightcontainer/RightContainer.tsx
@@ -1,13 +1,24 @@
 import SubscriptionCard from "./SubscriptionCard";
 import CalendarSliderCard from "./CalendarSliderCard";
 import { Card } from "@/components/ui/card";
+import { useGetUserQuery } from "@/services/api";
 
 export default function RightContainer() {
+  const { data: user, isLoading } = useGetUserQuery();
+
+  if (isLoading) return null;
+
   return (
     <div className="hidden w-[330px] flex-col gap-4 bg-white p-4 md:flex">
-      {/* Sub scription */}
+      {/* Subscription */}
       <Card className="border-none p-0 shadow-none">
-        <SubscriptionCard />
+        <SubscriptionCard
+          isPro={user.profile?.is_pro}
+          
+          
+          planName="Paket Premium"
+          expiredAt={user.profile?.end_date}
+        />
       </Card>
 
       {/* Calendar Slider */}
